f6188a0c4f966409caae8f5287b071e2eed08ccd
Indranil Pal <indranil328@gmail.com>
2022-07-13 14:11:44 -0400

    HW2 Part 1 and Part 2 WIP

--- Assignment2/Work/Indranil/hw2_part2.py
+++ Assignment2/Work/Indranil/hw2_part2.py
@@ -5,37 +5,39 @@ import math
 
 
 ################SVD with a random 3x2 matrix
-A = np.array([[-1,2],[3,-2],[5,7]])
-U,d,VT = np.linalg.svd(A)
+
+training_setA=[]
+
+def setup():
+    for i in range(datasetSize):
+        x1 = np.random.randint(-5, 5)
+        x2 = np.random.randint(-5, 5)
+        training_setA.append((x1, x2))
+
+datasetSize=3
+
+setup()
+
+print (training_setA)
+
+U,d,VT = np.linalg.svd(training_setA)
 
 print(' U: {} :  [d: {} , VT:{}]'.format(U.shape, d.shape, VT.shape ))
 
-
-#print(" U is \n" , U)
-#print(" d is \n " , d)
-#print(" V transpose is \n" , VT.T)
-#print(" V  is \n" , VT.T)
-#print(" U transpose is \n" , U.T)
 D  =  np.diag(d)
 print("D shape is ", D.shape)
 D_inver = np.linalg.inv(D)
 print(" D inverse shape {} : D shape :{}  \n ".format(D_inver.shape, D.shape))
 
-## D inverse is 2x2 , have to make it 2x3
 Dplus = np.concatenate((D_inver, np.array([[0,0]]).T),axis = 1)
-#print(" d plus \n ", Dplus.shape, Dplus)
-#print ("#####################")
-#print (VT.T , "\n", Dplus , "\n", U.T)
-#print ("#####################")
+
 Aplus = np.dot(VT.T, np.dot(Dplus,U.T))
 print("Inverse matrix shape is " , Aplus.shape)
-#print("Inverse matrix  is \n" , Aplus)
-#----------verify
-print(" A multiplies by A T : Should be an identity matrix \n ", np.dot(Aplus, A))
-#print (" V shape is ", VT.T)
 
+#----------verify
+print(" A multiplies by A T : Should be an identity matrix \n ", np.dot(Aplus, training_setA))
 print("Pseudo inverse not using np library: \n ", Aplus)
-print("Pseudo inverse using np library: \n ", np.linalg.pinv(A))
+print("Pseudo inverse using np library: \n ", np.linalg.pinv(training_setA))
 
 ########## END OF Sample matrix
 
